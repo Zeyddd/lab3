92ead9855d2ad0051e3d3d9cbb1c99229aeace3d
Zeyddd <zeyad.almoaigel@gmail.com>
2022-10-13 12:55:50 -0700

    fixed reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,47 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input1 = {3,1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{1,3}, input1);
+	}
+
+  @Test 
+	public void testReverseInPlace3() {
+    int[] input1 = {3,2,1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{1,2,3}, input1);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed3() {
+    int[] input1 = {0,1,0};
+    assertArrayEquals(new int[]{0,1,0}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed4() {
+    int[] input1 = {0,1,1};
+    assertArrayEquals(new int[]{1,1,0}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed5() {
+    int[] input1 = {0,1};
+    assertArrayEquals(new int[]{1,0}, ArrayExamples.reversed(input1));
   }
 }
